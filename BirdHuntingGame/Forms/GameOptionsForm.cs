b85415e9d54c17edb93ece4b8b6e14419046ba0f
Sajjad Gul <sajjadarifgul@gmail.com>
2018-03-04 22:19:48 +0500

    Updated Crosshair and Rendering

--- BirdHuntingGame/Forms/GameOptionsForm.cs
+++ BirdHuntingGame/Forms/GameOptionsForm.cs
@@ -35,6 +35,16 @@ namespace BirdHuntingGame.Forms
 		}
 		#endregion
 
+		protected override CreateParams CreateParams
+		{
+			get
+			{
+				CreateParams cp = base.CreateParams;
+				cp.ExStyle |= 0x02000000;  // Turn on WS_EX_COMPOSITED
+				return cp;
+			}
+		}
+
 		private void GameOptionsForm_Load(object sender, EventArgs e)
 		{
 			cmbGun.SelectedIndex = 0;
